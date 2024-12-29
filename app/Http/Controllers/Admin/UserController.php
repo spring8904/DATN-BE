f07cbe8ea16a4e0d852b7fa1eb84a0ce4a98088f
anh23256 <anhldph45102@fpt.edu.vn>
2024-12-30 00:38:43 +0700

    feature/admin-user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,16 +3,47 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Admin\Users\StoreUserRequest;
+use App\Http\Requests\Admin\Users\UpdateUserRequest;
+use App\Models\User;
+use App\Traits\LoggableTrait;
+use App\Traits\UploadToCloudinaryTrait;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
+    use LoggableTrait, UploadToCloudinaryTrait;
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        try {
+
+            $title = 'Quản lý thành viên';
+            $subTitle = 'Danh sách người dùng';
+
+            $users = User::query()->latest('id')->paginate(10);
+            $countUsers = User::query()->count('id');
+            $userActive = User::query()->where('status', 'active')->count('id');
+            $userInActive = User::query()->where('status', 'inactive')->count('id');
+            $userBlocked = User::query()->where('status', 'blocked')->count('id');
+
+            return view('users.index', compact([
+                'users',
+                'userActive',
+                'userInActive',
+                'userBlocked',
+                'countUsers',
+                'subTitle',
+                'title'
+            ]));
+        } catch (\Exception $e) {
+
+            $this->logError($e);
+
+            return redirect()->back()->with('error', 'Có lỗi xảy ra, vui lòng thử lại sau');
+        }
     }
 
     /**
@@ -20,15 +51,53 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        try {
+
+            $title = 'Quản lý thành viên';
+            $subTitle = 'Thêm mới người dùng';
+
+            return view('users.create', compact([
+                'title',
+                'subTitle'
+            ]));
+        } catch (\Exception $e) {
+
+            $this->logError($e);
+
+            return redirect()->back()->with('error', 'Có lỗi xảy ra, vui lòng thử lại sau');
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
-        //
+        try {
+
+            $validator = $request->validated();
+
+            $data = $request->except('avatar');
+
+            if ($request->hasFile('avatar')) {
+                $data['avatar'] = $this->uploadImage($request->file('avatar'));
+            }
+
+            $data['code'] = str_replace('-', '', Str::uuid()->toString());
+
+            User::query()->create($data);
+
+            return redirect()->route('admin.users.index')->with('success', true);
+        } catch (\Exception $e) {
+
+            if (isset($data['avatar']) && !empty($data['avatar']) && filter_var($data['avatar'], FILTER_VALIDATE_URL)) {
+                $this->deleteImage($data['avatar']);
+            }
+
+            $this->logError($e);
+
+            return redirect()->back()->with('error', 'Có lỗi xảy ra, vui lòng thử lại sau');
+        }
     }
 
     /**
@@ -36,7 +105,24 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+
+            $title = 'Quản lý thành viên';
+            $subTitle = 'Chi tiết người dùng';
+
+            $user = User::query()->findOrFail($id);
+
+            return view('users.show', compact([
+                'user',
+                'title',
+                'subTitle'
+            ]));
+        } catch (\Exception $e) {
+
+            $this->logError($e);
+
+            return redirect()->back()->with('error', 'Có lỗi xảy ra, vui lòng thử lại sau');
+        }
     }
 
     /**
@@ -44,15 +130,66 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        try {
+
+            $title = 'Quản lý thành viên';
+            $subTitle = 'Cập nhật người dùng';
+
+            $user = User::query()->findOrFail($id);
+
+            return view('users.edit', compact([
+                'user',
+                'title',
+                'subTitle'
+            ]));
+        } catch (\Exception $e) {
+
+            $this->logError($e);
+
+            return redirect()->back()->with('error', 'Có lỗi xảy ra, vui lòng thử lại sau');
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateUserRequest $request, string $id)
     {
-        //
+        try {
+
+            $validator = $request->validated();
+
+            $data = $request->except('avatar');
+
+            $user = User::query()->findOrFail($id);
+
+            $currencyAvatar = $user->avatar;
+
+            if ($request->hasFile('avatar')) {
+                $data['avatar'] = $this->uploadImage($request->file('avatar'));
+            }
+
+            $user->update($data);
+
+            if (
+                isset($data['avatar']) && !empty($data['avatar'])
+                && filter_var($data['avatar'], FILTER_VALIDATE_URL)
+                && !empty($currencyAvatar)
+            ) {
+                $this->deleteImage($currencyAvatar);
+            }
+
+            return redirect()->route('admin.users.edit', $id)->with('success', true);
+        } catch (\Exception $e) {
+
+            if (isset($data['avatar']) && !empty($data['avatar']) && filter_var($data['avatar'], FILTER_VALIDATE_URL)) {
+                $this->deleteImage($data['avatar']);
+            }
+
+            $this->logError($e);
+
+            return redirect()->back()->with('error', 'Có lỗi xảy ra, vui lòng thử lại sau');
+        }
     }
 
     /**
@@ -60,6 +197,53 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+
+            if (str_contains($id, ',')) {
+
+                $userID = explode(',', $id);
+
+                $this->deleteUsers($userID);
+            } else {
+                $user = User::query()->findOrFail($id);
+
+                $data['avatar'] = $user->avatar;
+
+                $user->delete();
+                if (isset($data['avatar']) && !empty($data['avatar']) && filter_var($data['avatar'], FILTER_VALIDATE_URL)) {
+                    $this->deleteImage($data['avatar']);
+                }
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Xóa thành công'
+            ]);
+        } catch (\Exception $e) {
+
+            $this->logError($e);
+
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Xóa thất bại'
+            ]);
+        }
+    }
+
+    private function deleteUsers(array $userID)
+    {
+
+        $users = User::query()->whereIn('id', $userID)->get();
+
+        foreach ($users as $user) {
+
+            $avatar = $user->avatar;
+
+            $user->delete();
+
+            if (isset($avatar) && !empty($avatar) && filter_var($avatar, FILTER_VALIDATE_URL)) {
+                $this->deleteImage($avatar);
+            }
+        }
     }
 }
