b036d9988b26ad5b37983c27cd94b18d80502b3c
TungTvPh81102 <quaixe121811@gmail.com>
2025-01-04 05:54:19 +0700

    feature/course-api

--- app/Http/Controllers/API/Instructor/CourseController.php
+++ app/Http/Controllers/API/Instructor/CourseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\Instructor;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Courses\StoreCourseRequest;
+use App\Http\Requests\API\Courses\UpdateContentCourse;
 use App\Models\Course;
 use App\Traits\LoggableTrait;
 use App\Traits\UploadToCloudinaryTrait;
@@ -11,20 +12,78 @@ use App\Traits\UploadToMuxTrait;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class CourseController extends Controller
 {
     use LoggableTrait, UploadToCloudinaryTrait, UploadToMuxTrait;
 
+    const FOLDER_COURSE_THUMBNAIL = 'courses/thumbnail';
+    const FOLDER_COURSE_INTRO = 'courses/intro';
+
+    public function getCourseOverView(string $slug)
+    {
+        try {
+            $course = Course::query()
+                ->where('slug', $slug)
+                ->select([
+                    'id',
+                    'category_id',
+                    'name',
+                    'slug',
+                    'thumbnail',
+                    'intro',
+                    'price',
+                    'price_sale',
+                    'description',
+                    'duration',
+                    'level',
+                    'total_student',
+                    'requirements',
+                    'benefits',
+                    'qa'
+                ])
+                ->first();
+
+            if (!$course) {
+                return $this->respondNotFound('Không tìm thấy khoá học');
+            }
+
+            $course->load([
+                'category' => function ($query) {
+                    $query->select('id', 'name', 'slug', 'parent_id');
+                }
+            ]);
+
+            return $this->respondOk('Thông tin khoá học: ' . $course->name,
+                $course
+            );
+        } catch (\Exception $e) {
+
+            $this->logError($e);
+            return $this->respondServerError('Có lỗi xảy ra, vui lòng thử lại',
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
     public function store(StoreCourseRequest $request)
     {
-        $data = $request->validated();
-        $data['user_id'] = 4;
-        $data['code'] = Str::uuid();
-        $data['slug'] = !empty($data['name']) ? Str::slug($data['name']) . '-' . $data['code'] : null;
-
         try {
+            $data = $request->validated();
+            $data['user_id'] = 4;
+
+            do {
+                $data['code'] = (string)Str::uuid();
+                $exits = Course::query()->where('code', $data['code'])->exists();
+            } while ($exits);
+
+            $data['slug'] = !empty($data['name'])
+                ? Str::slug($data['name']) . '-' . $data['code']
+                : $data['code'];
+
             $course = Course::query()->create($data);
 
             return $this->respondCreated('Tạo khoá học thành công', [
@@ -36,6 +95,114 @@ class CourseController extends Controller
             return $this->respondServerError('Có lỗi xảy ra, vui lòng thử lại',
                 Response::HTTP_INTERNAL_SERVER_ERROR
             );
+        }
+    }
+
+    public function updateContentCourse(UpdateContentCourse $request, string $slug)
+    {
+        try {
+            DB::beginTransaction();
+
+            $data = $request->validated();
+
+            $course = Course::query()
+                ->where('slug', $slug)
+                ->first();
+
+            $thumbnailOld = $course->thumbnail;
+            $introOld = $course->intro;
+
+            if (!$course) {
+                return $this->respondNotFound('Không tìm thấy khoá học');
+            }
+
+            $data['slug'] = !empty($data['name'])
+                ? Str::slug($data['name']) . '-' . $course->code
+                : $course->slug;
+
+            $data['thumbnail'] = $request->hasFile('thumbnail')
+                ? $this->handleFileUpload(
+                    $request->file('thumbnail'),
+                    $thumbnailOld,
+                    self::FOLDER_COURSE_THUMBNAIL,
+                    'image')
+                : $thumbnailOld;
+
+            $data['intro'] = $request->hasFile('intro')
+                ? $this->handleFileUpload(
+                    $request->file('intro'),
+                    $introOld,
+                    self::FOLDER_COURSE_INTRO,
+                    'video')
+                : $introOld;
+
+            $data['requirements'] = !empty($request->input('requirements'))
+                ? json_encode($request->input('requirements'))
+                : $course->requirements;
+            $data['benefits'] = !empty($request->input('benefits'))
+                ? json_encode($request->input('benefits'))
+                : $course->benefits;
+            $data['qa'] = !empty($request->input('qa'))
+                ? json_encode($request->input('qa'))
+                : $course->qa;
+
+            $course->update($data);
+
+            DB::commit();
+
+            return $this->respondOk('Thao tác thành công',
+                $course->load('category')
+            );
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            $this->rollbackFileUploads($data, $thumbnailOld, $introOld);
+
+            $this->logError($e);
+
+            if ($e instanceof ValidationException) {
+                return $this->respondFailedValidation('Dữ liệu không hợp lệ', $e->errors());
+            }
+
+            return $this->respondServerError('Có lỗi xảy ra, vui lòng thử lại',
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    private function handleFileUpload($newFile, $oldFile, $folder, $type)
+    {
+        $uploadFile = $type === 'image'
+            ? $this->uploadImage($newFile, $folder)
+            : $this->uploadVideo($newFile, $folder);
+
+        if (!empty($oldFile) && filter_var($oldFile, FILTER_VALIDATE_URL)) {
+            $type === 'image'
+                ? $this->deleteImage($oldFile, $folder)
+                : $this->deleteVideo($oldFile, $folder);
+        }
+
+        return $uploadFile;
+    }
+
+    private function rollbackFileUploads(array $data, $thumbnailOld, $introOld)
+    {
+        if (!empty($data['thumbnail']) && filter_var($data['thumbnail'], FILTER_VALIDATE_URL)) {
+            $this->deleteImage($data['thumbnail'], self::FOLDER_COURSE_THUMBNAIL);
+        }
+
+        if (!empty($data['intro']) && filter_var($data['intro'], FILTER_VALIDATE_URL)) {
+            $this->deleteVideo($data['intro'], self::FOLDER_COURSE_INTRO);
+        }
+
+        $this->deleteFileIfValid($thumbnailOld, self::FOLDER_COURSE_THUMBNAIL, 'image');
+        $this->deleteFileIfValid($introOld, self::FOLDER_COURSE_INTRO, 'video');
+    }
+
+    private function deleteFileIfValid($file, $folder, $type)
+    {
+        if (!empty($file) && filter_var($file, FILTER_VALIDATE_URL)) {
+            $type === 'image' ? $this->deleteImage($file, $folder) : $this->deleteVideo($file, $folder);
         }
     }
 }
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -27,13 +27,14 @@ class Course extends Model
         'name',
         'slug',
         'thumbnail',
+        'intro',
         'price',
         'price_sale',
         'description',
         'duration',
         'level',
         'total_student',
-        'requirement',
+        'requirements',
         'benefits',
         'qa',
         'status',
@@ -57,5 +58,10 @@ class Course extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
