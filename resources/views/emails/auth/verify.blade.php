f391680267f98c5637b9ee68a4f9582b6b1ba595
longnt <longnt2k4@gmail.com>
2025-02-12 18:18:31 +0700

    add-view-verify

--- resources/views/emails/auth/verify.blade.php
+++ resources/views/emails/auth/verify.blade.php
@@ -1,71 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Generate QR Code</title>
-</head>
-<body>
-<h1>Generate QR Code</h1>
+@extends('layouts.auth')
 
-<!-- Form for submitting QR code data -->
-<form action="{{ route('qr.generate') }}" method="POST">
-    @csrf
-    <div>
-        <label for="accountNo">Account No:</label>
-        <input type="text" name="accountNo" id="accountNo" required value="{{ old('accountNo') }}">
-        @error('accountNo') <span>{{ $message }}</span> @enderror
+@section('title', 'Xác minh email')
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <table class="body-wrap" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background-color: transparent; margin: 0;">
+            <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                <td style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;" valign="top"></td>
+                <td class="container" width="600" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto;" valign="top">
+                    <div class="content" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;">
+                        <table class="main" width="100%" cellpadding="0" cellspacing="0" itemprop="action" itemscope itemtype="http://schema.org/ConfirmAction" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; margin: 0; border: none;">
+                            <tr style="font-family: 'Roboto', sans-serif; font-size: 14px; margin: 0;">
+                                <td class="content-wrap" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; color: #495057; font-size: 14px; vertical-align: top; margin: 0;padding: 30px; box-shadow: 0 3px 15px rgba(30,32,37,.06); ;border-radius: 7px; background-color: #fff;" valign="top">
+                                    <meta itemprop="name" content="Confirm Email" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;" />
+                                    <table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                        <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                            <td class="content-block" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+                                                <div style="margin-left: 120px">
+                                                    <a style="display: flex;align-items: center; gap: 10px" href="index.html"
+                                                        class="">
+                                                        <img src="{{ asset('assets/images/logo-container.png') }}" alt=""
+                                                            width="50" height="50">
+                                                        <span class="custom-text-logo text-primary">CourseHub</span>
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                            <td class="content-block" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 24px; vertical-align: top; margin: 0; padding: 0 0 10px;  text-align: center;" valign="top">
+                                                <h4 style="font-family: 'Roboto', sans-serif; font-weight: 500;">Vui lòng xác minh email của bạn</h5>
+                                            </td>
+                                        </tr>
+                                        <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                            <td class="content-block" style="font-family: 'Roboto', sans-serif; color: #878a99; box-sizing: border-box; font-size: 15px; vertical-align: top; margin: 0; padding: 0 0 26px; text-align: center;" valign="top">
+                                                <p style="margin-bottom: 0;">Vui lòng xác thực địa chỉ email của bạn để truy cập trang quản trị.</p>
+                                            </td>
+                                        </tr>
+                                        <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                            <td class="content-block" itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 22px; text-align: center;" valign="top">
+                                                <a href="#" itemprop="url" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: .8125rem; color: #FFF; text-decoration: none; font-weight: 400; text-align: center; cursor: pointer; display: inline-block; border-radius: .25rem; text-transform: capitalize; background-color: #405189; margin: 0; border-color: #405189; border-style: solid; border-width: 1px; padding: .5rem .9rem;">Xác minh email của bạn</a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <!-- end table -->
     </div>
-
-    <div>
-        <label for="accountName">Account Name:</label>
-        <input type="text" name="accountName" id="accountName" required value="{{ old('accountName') }}">
-        @error('accountName') <span>{{ $message }}</span> @enderror
-    </div>
-
-    <div>
-        <label for="acqId">Acquirer ID:</label>
-        <input type="text" name="acqId" id="acqId" required value="{{ old('acqId') }}">
-        @error('acqId') <span>{{ $message }}</span> @enderror
-    </div>
-
-    <div>
-        <label for="amount">Amount:</label>
-        <input type="text" name="amount" id="amount" required value="{{ old('amount') }}">
-        @error('amount') <span>{{ $message }}</span> @enderror
-    </div>
-
-    <div>
-        <label for="addInfo">Additional Info:</label>
-        <input type="text" name="addInfo" id="addInfo" value="{{ old('addInfo') }}">
-    </div>
-
-    <div>
-        <label for="format">Format:</label>
-        <select name="format" id="format" required>
-            <option value="text" {{ old('format') == 'text' ? 'selected' : '' }}>Text</option>
-            <option value="html" {{ old('format') == 'html' ? 'selected' : '' }}>HTML</option>
-        </select>
-    </div>
-
-    <div>
-        <label for="template">Template:</label>
-        <select name="template" id="template" required>
-            <option value="compact" {{ old('template') == 'compact' ? 'selected' : '' }}>Compact</option>
-            <option value="compact2" {{ old('template') == 'compact' ? 'selected' : '' }}>Compact2</option>
-            <option value="detailed" {{ old('template') == 'detailed' ? 'selected' : '' }}>Detailed</option>
-        </select>
-    </div>
-
-    <button type="submit">Generate QR Code</button>
-</form>
-
-@if(isset($qrDataURL))
-    <h2>Your QR Code:</h2>
-    <div>
-        <!-- Display generated QR code as image -->
-        <img src="{{$qrDataURL }}" alt="QR Code">
-    </div>
-@endif
-</body>
-</html>
+    <!--end col-->
+</div><!-- end row -->
+@endsection
